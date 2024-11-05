be5bc0999e21952bc1d692b65946b3bb9d5591eb
Rodrigo D <neonrok@pm.me>
2024-11-05 17:41:55 +0000

    inicio a forma de guardar os pontos

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
+import * as arc from "./models/registroPontos.js";
+
 let wid = 600;
 let hei = 450;
+
 let x = 0;
 let y = 0;
+//let arca = [];
 
 let c = document.querySelector(".cava");
 c.width = wid;
 c.height = hei;
 
-let cav = c.getContext("2d");
-
+//éstas funções servem apenas para desmontar alguns processoa para que possam ser reutilizados
 const mapCordsX = function(){
-    c.addEventListener('click', e =>{
+    c.addEventListener('click', e => {
         x = e.offsetX;
     });
     return x;
 };
-
 const mapCordsY = function(){
-    c.addEventListener('click', e =>{
+    c.addEventListener('click', e => {
         y = e.offsetY;
     });
     return y;
 };
+const sav = function(x, y){
+    
+    let addArc = new arc(z, x, y)
 
+}
+
+
+let cav = c.getContext("2d");
 
 
---
+++ js/models/registroPontos.js
@@ -0,0 +1,11 @@
+
+export class ponts{
+    Id = 0;
+    x = 0;
+    y = 0;
+    constructor(Id, x, y){
+        this.Id = Id;
+        this.x = x;
+        this.y = y;
+    }
+};
