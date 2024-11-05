6e07f5733f83887b648c85ada5ffba75e466f4bd
Rodrigo D <neonrok@pm.me>
2024-11-05 12:24:21 +0000

    Funções das cordenadas

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 let wid = 600;
 let hei = 450;
+let x = 0;
+let y = 0;
 
 let c = document.querySelector(".cava");
 c.width = wid;
 c.height = hei;
 
-let ctb = c.getContext("2d");
+let cav = c.getContext("2d");
+
+const mapCordsX = function(){
+    c.addEventListener('click', e =>{
+        x = e.offsetX;
+    });
+    return x;
+};
+
+const mapCordsY = function(){
+    c.addEventListener('click', e =>{
+        y = e.offsetY;
+    });
+    return y;
+};
+
+
+
